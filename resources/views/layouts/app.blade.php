f6606ad1e0b89009f748cc4cabc2116e6d45558b
vasily.smolin <vasilysmoolin@gmail.com>
2021-09-27 15:28:13 +0400

    main route and welcome blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Анализатор страниц</title>
+    <meta name="description" content="Анализатор страниц">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="min-vh-100 d-flex flex-column">
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="{{ route('main') }}">Анализатор страниц</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('main') }}">Главная</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Сайты</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<main class="flex-grow-1">
+    @include('flash::message')
+    @yield('content')
+</main>
+
+<footer class="border-top py-3 mt-5">
+    <div class="container-lg">
+        <div class="text-center">
+            <a href="https://hexlet.io/pages/about" target="_blank">Hexlet</a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', ['as' => 'main', function (Request $request): object {
     return view('welcome');
-});
+}]);
