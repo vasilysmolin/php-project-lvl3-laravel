630afcd1380f1186d3c96d7facf056c82b7f2a9b
vasily.smolin <vasilysmoolin@gmail.com>
2021-10-09 18:03:14 +0400

    check url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,24 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('main') }}">Главная</a>
+
+                        <a class="nav-link
+                           @if(Illuminate\Support\Facades\URL::current() === route('main'))
+                               active
+                           @endif
+                            "
+                           href="{{ route('main') }}">Главная</a>
+
                 </li>
-                <li class="nav-item">
+                <li class="nav-item
+                           @if(Illuminate\Support\Facades\URL::current() === route('urls.index'))
+                            active
+                            @endif
+                    ">
                     <a class="nav-link" href="{{ route('urls.index') }}">Сайты</a>
                 </li>
             </ul>
